a1accb148a49c20d4e6ecce3e72f42c398f841c4
Eduardo Lazaro <grupohemsa@gmail.com>
2026-04-23 11:15:14 +0200

    Refa: Se comvierten en lazy las rutas de mapeo del app.routes.

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -4,28 +4,30 @@ import { Login } from './paginas/public/login/login';
 import { Registro } from './paginas/public/registro/registro';
 import { RecuperarPassword } from './paginas/public/recuperar-password/recuperar-password';
 
+/* Asocia una ruta (path) con un Componente (mapa de rutas) */
 export const routes: Routes = [
   {
-    path: '',
+    path: '', // Ruta RAIZ (home), se referencia con routerlink="" en el html
     component: Inicio,
-    title: 'Página de inicio'
+    title: 'Página de inicio' // titulo de la pestana del navegador, UX y SEO
   },
   {
     path: 'login',
-    component: Login,
+    // component: Login, // Carga directa sin lazy loading
+    loadComponent: () => import('./paginas/public/login/login').then(comp => comp.Login), //carga lazy loading del componente, se carga solo cuando se accede a la ruta
     title: 'Iniciar sesión'
   },
   {
     path: 'registro',
-    component: Registro,
+    loadComponent: () => import('./paginas/public/registro/registro').then(comp => comp.Registro),
     title: 'Registro'
   },
   {
     path: 'recuperar-password',
-    component: RecuperarPassword
-  },
+    loadComponent: () => import('./paginas/public/recuperar-password/recuperar-password').then(comp => comp.RecuperarPassword),
+    title: 'Recuperar contraseña'},
   {
-    path: '**',
-    redirectTo: ''
+    path: '**', // ruta wildcard, se activa si no se encuentra ninguna ruta coincidente
+    redirectTo: '' // Redirige a la ruta raíz (home) si no se encuentra la ruta solicitada
   }
 ];
