573f85de18316c8f34e0674ae02c7abf9d73841c
Eduardo Lazaro <grupohemsa@gmail.com>
2026-04-27 19:59:23 +0200

    Feat: Terminar la pagina de contacto y crear header-footer para diferentes usuarios.

--- FrontEnd/src/app/paginas/public/contacto/contacto.ts
+++ FrontEnd/src/app/paginas/public/contacto/contacto.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Location } from '@angular/common';
+import { Header } from '../../../components/header/header';
+import { Footer } from "../../../components/footer/footer";
+import { RouterLink, RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [FormsModule],       // <-- SoloFormsModule
+  imports: [FormsModule, Header, Footer, RouterLink, RouterModule],       // <-- SoloFormsModule
   templateUrl: './contacto.html',
   styleUrl: './contacto.css',
 })
